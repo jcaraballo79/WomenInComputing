8862bad82eae288ecea83be80f05f5089dad1019
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-02-25 21:40:06 -0500

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Jorge Caraballo on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
